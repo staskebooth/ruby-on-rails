c09527fe2d0684cf3bee9373acfde4af2573e9f2
staskebooth <jstaske@chicagobooth.edu>
2021-05-26 01:44:06 +0000

    First commit during setup.  Tried to install initial gems

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,10 +1,9 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get welcome_index_url
     assert_response :success
-    assert_select "body", "You're on Rails!"
+    assert_select 'body', "You're on Rails!"
   end
-
 end
