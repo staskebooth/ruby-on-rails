7b73c74661fdf05346095c6e771a74f4ec7526b7
staskebooth <jstaske@chicagobooth.edu>
2021-05-26 02:46:30 +0000

    forgot rails db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_024156) do
+ActiveRecord::Schema.define(version: 2021_05_26_024503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.integer "calories"
+    t.integer "protien"
+    t.integer "carbohydrate"
+    t.integer "fat"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "meals", force: :cascade do |t|
     t.string "meal_name"
