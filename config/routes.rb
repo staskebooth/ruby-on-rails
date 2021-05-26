0b1f0420f26bf05d08787f73b0f6289033ac8072
staskebooth <jstaske@chicagobooth.edu>
2021-05-26 02:45:15 +0000

    forgot to also add ingredients via scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@
 #      rails_direct_uploads POST /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
 
 Rails.application.routes.draw do
+  resources :ingredients
   resources :meals
   resources :users
   get 'welcome/index'
